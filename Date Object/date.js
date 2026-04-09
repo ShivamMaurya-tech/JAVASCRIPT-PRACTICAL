e89b09c8dfad7e3cbe3b92ad1536b574e0a834e4
Shivam Maurya <shivammaurya224466@gmail.com>
2026-04-10 00:00:42 +0530

    updating content

---
+++ Date Object/date.js
@@ -0,0 +1,41 @@
+// const time=new Date();
+// console.log(time);
+
+
+// // const fullyear=time.getFullYear()
+// // // console.log(fullyear)
+
+// // const months=time.getMonth()
+// // // console.log(months)
+
+// // const day=time.setDate(20);
+// // // console.log(day)
+
+// // const milli=time.getDay(1775748424642)
+// // console.log(milli)
+
+// // time.setHours(30)
+// // console.log(time)
+
+// const localstring=time.toLocaleString()
+// console.log(localstring);
+
+// time.setHours(30)
+// console.log(time)
+
+
+// time.toLocaleString()
+// console.log(times);
+
+// Q-write a function to add a specified number of days to given date
+const date=new Date("2026-04-09");
+console.log(date)
+
+const numberdays=(date,7)=>{
+
+    console.log(numberdays.toLocaleString)
+
+
+    
+
+}
